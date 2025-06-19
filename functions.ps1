c3e40bab3837bda1d8c53adfa06cd41c90a951b4
mgeneral-insight <109831810+mgeneral-insight@users.noreply.github.com>
2025-06-19 16:14:59 -0400

    Update functions.ps1

--- functions.ps1
+++ functions.ps1
@@ -1,3 +1,6 @@
+$ScriptPath = "C:\Scripts\Insight"
+
+
 function LogMessage {
     param (
         [string]$Message,
@@ -9,4 +12,36 @@ function LogMessage {
     if ($Err) { Write-Host $LogEntry -ForegroundColor Red }
     else { Write-Host $LogEntry }
     Add-Content -Path $LogFile -Value $LogEntry -Append
+}
+function UpdateScript {
+    param (
+        [string]$ScriptName
+    )
+    if (!(Test-Path -path "$ScriptPath\temp")) { $path = New-Item -Path "$scriptPath\temp" -ItemType Directory }
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mgeneral-insight/DURA-WindowsScripts/main/$ScriptName" -OutFile "$ScriptPath\temp\$ScriptName"
+    $LatestHash = Get-FileHash -Path "$ScriptPath\temp\$ScriptName"
+    if (!(Test-Path -Path "$ScriptPath\$ScriptName")) {
+        $CurrentHash = "NULL"
+    } else {
+        $CurrentHash = Get-FileHash -Path "$ScriptPath\$ScriptName"
+    }
+    if ($CurrentHash -ne $LatestHash) {
+        Write-Host "Running script is not latest version, updating and restarting script."
+        Copy-Item -Path "$ScriptPath\temp\$ScriptName" -Destination "$ScriptPath\$ScriptName" -Recurse
+    }
+}
+function UpdateFunctions {
+    if (!(Test-Path -path "$ScriptPath\temp")) { $path = New-Item -Path "$scriptPath\temp" -ItemType Directory }
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mgeneral-insight/DURA-WindowsScripts/main/functions.ps1" -OutFile "$ScriptPath\temp\functions.ps1"
+    $LatestHash = Get-FileHash -Path "$ScriptPath\temp\functions.ps1"
+    if (!(Test-Path -Path "$ScriptPath\functions.ps1")) {
+        $CurrentHash = "NULL"
+    } else {
+        $CurrentHash = Get-FileHash -Path "$ScriptPath\functions.ps1"
+    }
+    if ($CurrentHash -ne $LatestHash) {
+        Write-Host "Running script is not latest version, updating and restarting script."
+        Copy-Item -Path "$ScriptPath\temp\functions.ps1" -Destination "$ScriptPath\functions.ps1" -Recurse
+    }
+
 }
