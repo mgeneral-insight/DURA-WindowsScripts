63631a8718259bca821249da7a32a2a0c035d6de
mgeneral-insight <109831810+mgeneral-insight@users.noreply.github.com>
2025-06-19 17:20:06 -0400

    Update functions.ps1

--- functions.ps1
+++ functions.ps1
@@ -1,17 +1,27 @@
 $ScriptPath = "C:\Scripts\Insight"
-
+$callStack = Get-PSCallStack
+if ($callStack | Where-Object ScriptName) {
+    $topLevelScriptPath = ($callStack | Where-Object ScriptName).ScriptName[-1]
+    $topLevelScript = (Get-Item -Path $topLevelScriptPath).Name
+    $topLevelScriptBase = (Get-Item -Path $topLevelScriptPath).BaseName
+}
 
 function LogMessage {
     param (
         [string]$Message,
-        [string]$LogFile,
-        [bool]$Err
+        [string]$Severity = "Info"
     )
     $TimeStamp = Get-Date
-    $LogEntry = "$TimeStamp - $Message"
-    if ($Err) { Write-Host $LogEntry -ForegroundColor Red }
-    else { Write-Host $LogEntry }
+    if (!(Test-Path -path "$ScriptPath\Logs")) { $path = New-Item -Path "$scriptPath\Logs" -ItemType Directory }
+    $LFTimeStamp = Get-Date -Format "yyyyMMdd"
+    $LogFile = "$ScriptPath\Logs\$LFTimeStamp-$topLevelScriptBase.log"
+    $Severity = "[$Severity]"
+    $LogEntry = "$TimeStamp : $Severity : $Message"
+    if ($Severity -eq "Error") { Write-Host $LogEntry -ForegroundColor Red }
+    elseif ($Severity -eq "Warn") { Write-Host $LogEntry -ForegroundColor Yellow }
+    elseif ($Severity -eq "Info") { Write-Host $LogEntry }
     Add-Content -Path $LogFile -Value $LogEntry -Append
+
 }
 function UpdateScript {
     param (
@@ -19,11 +29,11 @@ function UpdateScript {
     )
     if (!(Test-Path -path "$ScriptPath\temp")) { $path = New-Item -Path "$scriptPath\temp" -ItemType Directory }
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mgeneral-insight/DURA-WindowsScripts/main/$ScriptName" -OutFile "$ScriptPath\temp\$ScriptName"
-    $LatestHash = Get-FileHash -Path "$ScriptPath\temp\$ScriptName"
+    $LatestHash = (Get-FileHash -Path "$ScriptPath\temp\$ScriptName").Hash
     if (!(Test-Path -Path "$ScriptPath\$ScriptName")) {
         $CurrentHash = "NULL"
     } else {
-        $CurrentHash = Get-FileHash -Path "$ScriptPath\$ScriptName"
+        $CurrentHash = (Get-FileHash -Path "$ScriptPath\$ScriptName").Hash
     }
     if ($CurrentHash -ne $LatestHash) {
         Write-Host "Running script is not latest version, updating and restarting script."
@@ -33,15 +43,14 @@ function UpdateScript {
 function UpdateFunctions {
     if (!(Test-Path -path "$ScriptPath\temp")) { $path = New-Item -Path "$scriptPath\temp" -ItemType Directory }
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mgeneral-insight/DURA-WindowsScripts/main/functions.ps1" -OutFile "$ScriptPath\temp\functions.ps1"
-    $LatestHash = Get-FileHash -Path "$ScriptPath\temp\functions.ps1"
+    $LatestHash = (Get-FileHash -Path "$ScriptPath\temp\functions.ps1").Hash
     if (!(Test-Path -Path "$ScriptPath\functions.ps1")) {
         $CurrentHash = "NULL"
     } else {
-        $CurrentHash = Get-FileHash -Path "$ScriptPath\functions.ps1"
+        $CurrentHash = (Get-FileHash -Path "$ScriptPath\functions.ps1").Hash
     }
     if ($CurrentHash -ne $LatestHash) {
         Write-Host "Running script is not latest version, updating and restarting script."
         Copy-Item -Path "$ScriptPath\temp\functions.ps1" -Destination "$ScriptPath\functions.ps1" -Recurse
     }
-
 }
