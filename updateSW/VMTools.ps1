e2f9ac3d912b6697bfadf100c572f1fd17a58e14
Insight Linux Util <michael.general@insight.com>
2025-06-30 16:14:14 -0400

    mg

--- updateSW/VMTools.ps1
+++ updateSW/VMTools.ps1
@@ -4,3 +4,4 @@ $updateString = 'C:\IT\$app.exe /S /l "c:\IT\$app_install.txt" /v "/qn REBOOT=R"
 function currentVersionCMD { (Get-WmiObject -Class win32_product -ComputerName $server | Where-Object Name -eq "VMware Tools").version }
 $installerPath = "\\azncwv078\IT-Packages\Application Install Packages\VMware Tools\latest\*.exe"
 $latestVersion = ((Get-ItemProperty $installerPath).VersionInfo).ProductVersion 
+ 
